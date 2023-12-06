eb3dc03ced4d89395c03acfc42a52739689e92c2
Zinouo <znd.odn@gmail.com>
2023-12-06 13:54:29 +0100

    TP05

--- deploy/api/controllers/utilisateur.controllers.js
+++ deploy/api/controllers/utilisateur.controllers.js
@@ -9,40 +9,39 @@ function generateAccessToken(user) {
 
 // Find a single Utilisateur with an login
 exports.login = (req, res) => {
-  const utilisateur = {
-    login: req.body.login,
-    password: req.body.password
-  };
+  const { login, password } = req.body;
 
-  // Test
-  let pattern = /^[A-Za-z0-9]{1,20}$/;
-  if (pattern.test(utilisateur.login) && pattern.test(utilisateur.password)) {
+  // Specific credentials to check against
+  const validLogin = "emma";
+  const validPassword = "toto";
 
-        const uuid = uuidv4 ();
-        const utilisateur = {
-          nom: "martin",
-          prenom: "jean",
-          login: "marsstin",
-          email : "martin.jean@gmail.com",
-          password : "toto",
-          id : uuid
-        };
+  if (login === validLogin && password === validPassword) {
+    const uuid = uuidv4();
+    const utilisateur = {
+      nom: "Martin",
+      prenom: "Jean",
+      login: "emma",
+      email: "martin.jean@gmail.com",
+      password: "toto",
+      id: uuid
+    };
 
-        const user = {
-          id: utilisateur.id,
-          name: utilisateur.nom,
-          email: utilisateur.email
-        };
-      
-        
-        let accessToken = generateAccessToken(user);
-        res.setHeader('Authorization', `Bearer ${accessToken}`);
+    const user = {
+      id: utilisateur.id,
+      name: utilisateur.nom,
+      email: utilisateur.email
+    };
 
-        console.log (accessToken);
-
-      
-        res.send(utilisateur);
-    };    
+    let accessToken = generateAccessToken(user);
+    res.setHeader('Authorization', `Bearer ${accessToken}`);
+    console.log(accessToken);
+    res.send(utilisateur);
+  } else {
+    // Send an appropriate response if login credentials are invalid
+    res.status(401).send("Invalid login credentials");
+  }
 };
+ 
+
 
 
